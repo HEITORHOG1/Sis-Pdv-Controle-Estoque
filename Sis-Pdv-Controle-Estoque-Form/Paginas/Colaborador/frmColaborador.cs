2ad2526870c2fd2128cf2c92c8f9dc49861a5278
Heitor Gonçalves <61200900+HEITORHOG1@users.noreply.github.com>
2025-06-19 19:45:48 -0300

    Fix collaborator status toggle (#17)

--- Sis-Pdv-Controle-Estoque-Form/Paginas/Colaborador/frmColaborador.cs
+++ Sis-Pdv-Controle-Estoque-Form/Paginas/Colaborador/frmColaborador.cs
@@ -52,7 +52,7 @@ namespace Sis_Pdv_Controle_Estoque_Form.Paginas.Colaborador
             {
                 rbInativo.Checked = true;
                 rbAtivo.Checked = false;
-                _ativo = true;
+                _ativo = false;
             }
 
 
@@ -153,7 +153,7 @@ namespace Sis_Pdv_Controle_Estoque_Form.Paginas.Colaborador
             }
             else
             {
-                _ativo = true;
+                _ativo = false;
             }
         }
         private async Task Excluir(ColaboradorDto dto)
